431154669dee4d5f5a8959e530c07873493e1c70
1Eva1987 <115692772+1Eva1987@users.noreply.github.com>
2023-01-12 12:58:43 +0000

    function showQuestion separated in two functions : showQuestion and playGame functions , time decreased by 10 if the answer is wrong, if time = 0 or all questions are answered game is finished

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -4,19 +4,21 @@ var quastionsDiv = document.querySelector("#questions");
 var quastTitle = document.querySelector("#question-title");
 var choices = document.querySelector(".choices");
 var timerEl = document.querySelector("#time");
-var newTime = 60;
+var newTime = 80;
 var quastionNumber = 0;
+var length = quastions.length;
 
 // timer function
 function timer() {
   var intervalTimer = setInterval(function () {
     newTime--;
     timerEl.textContent = newTime;
-    if (newTime === 0) {
+    if (newTime <= 0) {
       clearInterval(intervalTimer);
     }
   }, 1000);
 }
+
 // remove previous quastions
 function removeQuestions() {
   choices.innerHTML = "";
@@ -24,46 +26,86 @@ function removeQuestions() {
 
 // function to show quastion
 function showQuastioin() {
+  if (newTime <= 0) {
+    quastTitle.innerHTML = "";
+    removeQuestions();
+    console.log("STOOOOOP");
+    console.log(newTime);
+    return;
+  }
   quastTitle.innerHTML = quastions[quastionNumber].quastion;
-
   quastions[quastionNumber].answers.forEach(function (answer) {
     var button = document.createElement("button");
     button.textContent = answer.text;
     choices.appendChild(button);
   });
+}
+// trying create function GAME
+function playGame() {
   choices.addEventListener("click", function (e) {
     var element = e.target;
-    console.log(element);
-    console.log(document.body);
-    for (let i = 0; i < quastions[quastionNumber].answers.length; i++) {
-      if (element.textContent === quastions[quastionNumber].answers[i].text) {
-        console.log(quastions[quastionNumber].answers[i].correct);
-        var answerIs = quastions[quastionNumber].answers[i].correct;
-        if (answerIs) {
-          // how to remove previous quastion buttons?
-
-          removeQuestions();
-          console.log("win");
-          quastionNumber = quastionNumber + 1;
-          console.log(quastionNumber);
+    if ((element.parentElement = choices)) {
+      for (let i = 0; i < quastions[quastionNumber].answers.length; i++) {
+        if (element.textContent === quastions[quastionNumber].answers[i].text) {
+          var answerIs = quastions[quastionNumber].answers[i].correct;
+        }
+      }
+      if (answerIs === true) {
+        removeQuestions();
+        console.log("win");
+        quastionNumber++;
+        length--;
+        if (length > 0) {
+          console.log(length);
           showQuastioin();
         } else {
-          console.log("lost");
+          console.log("END");
+          quastTitle.innerHTML = "";
           removeQuestions();
-
-          //   time should be substracted by 10
-          quastionNumber = quastionNumber + 1;
-          console.log(quastionNumber);
+          console.log("STOOOOOP");
+          console.log(newTime);
+          newTime = 0;
+        }
+      } else if (answerIs === false) {
+        removeQuestions();
+        console.log("lost");
+        newTime = newTime - 10;
+        quastionNumber++;
+        length--;
+        if (length > 0) {
+          console.log(length);
           showQuastioin();
+        } else {
+          console.log("END");
+          quastTitle.innerHTML = "";
+          removeQuestions();
+          console.log("STOOOOOP");
+          console.log(newTime);
+
+          newTime = 0;
         }
       }
     }
   });
 }
+
 // event listiner
 startBtn.addEventListener("click", function () {
   timer();
   startScreen.setAttribute("class", "hide");
   quastionsDiv.removeAttribute("class");
+
   showQuastioin();
+  playGame();
 });
+// GIVEN I am taking a code quiz (✓)
+// WHEN I click the start button (✓)
+// THEN a timer starts and I am presented with a question (✓)
+// WHEN I answer a question(✓)
+// THEN I am presented with another question (✓)
+// WHEN I answer a question incorrectly(✓)
+// THEN time is subtracted from the clock(✓)
+// WHEN all questions are answered or the timer reaches 0
+// THEN the game is over
+// WHEN the game is over
+// THEN I can save my initials and score
