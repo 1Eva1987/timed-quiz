dae0c5c7b3b84d5c6b718519361802dfca4549cb
1Eva1987 <115692772+1Eva1987@users.noreply.github.com>
2023-01-12 09:46:02 +0000

    added more questions in questions.js file

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,7 +1,7 @@
 var quastions = [
   {
     quastion:
-      "Strin values has to be enclosed within _______ when being assigned to variables. ",
+      "String values has to be enclosed within _______ when being assigned to variables. ",
     answers: [
       { text: "1. commas", correct: false },
       { text: "2. quates", correct: true },
@@ -10,10 +10,41 @@ var quastions = [
     ],
   },
   {
-    quastion: "Comonly used data types DO Not Include:",
+    quastion: "Commonly used data types DO Not Include:",
     answers: [
-      { text: "1. alerts", correct: false },
-      { text: "2. numbers", correct: true },
+      { text: "1. strings", correct: false },
+      { text: "2. booleans", correct: false },
+      { text: "3. alerts", correct: true },
+      { text: "4. numbers", correct: false },
+    ],
+  },
+  {
+    quastion:
+      "The condition in an if / else statement is enclosed with_________.",
+    answers: [
+      { text: "1. quates", correct: false },
+      { text: "2. curly brackets", correct: false },
+      { text: "3. parentheses", correct: true },
+      { text: "4. square brackets", correct: false },
+    ],
+  },
+  {
+    quastion: "Arrays in JS can be used to store __________.",
+    answers: [
+      { text: "1. numbers and strings", correct: false },
+      { text: "2. other arrays", correct: false },
+      { text: "3. booleans", correct: true },
+      { text: "4. all of the above", correct: false },
+    ],
+  },
+  {
+    quastion:
+      "A very useful tool used during development and debugging for printing content to the debugger is :",
+    answers: [
+      { text: "1. JavaScript", correct: false },
+      { text: "2. terminal", correct: false },
+      { text: "3. for loops", correct: false },
+      { text: "4. console.log", correct: true },
     ],
   },
 ];
